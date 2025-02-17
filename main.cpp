7fec80105b106dea5ed6d8d21a3b2a27747903b6
Yainelly <yainelly.ortiz@upr.edu>
2025-02-17 21:28:34 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
-// escriba su nombre, username en github y numero de estudiante en el header
-// incluya cualquier colaboración que haga
-#include<iostream>
+// Yainelly Ortiz Serrano, @Yainelly 
+// 801-23-1813
+// Helpers: Chat GPT
+
+#include <iostream>
 using namespace std;
- 
+
 int main(){
-  // escriba su código aquí 
+  
+  int a, b, c, d; 
+  char slash;
+
+  cout << "En este programa sumaremos dos fracciones.\n ";
+  cout << "Escriba dos fracciones de forma a/b\n"; 
+
+  cout << "Fraccion 1: ";
+  cin >> a >> slash >> b;
+
+  cout << "Fraccion 2: "; 
+  cin >> c >> slash >> d; 
+
+  int numerador = (a * d) + (b * c);
+  int denominador = (b * d); 
+
+  cout << "La suma de estas dos fracciones es: " << numerador << "/" << denominador << "\n"; 
   
   return 0;
 }
