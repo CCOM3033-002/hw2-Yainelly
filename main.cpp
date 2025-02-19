0e1489df145b21221dd8156414b80fb81a8b65f2
Yainelly <yainelly.ortiz@upr.edu>
2025-02-19 17:54:27 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,21 +7,31 @@ using namespace std;
 
 int main(){
   
+  // le damos valor a las variables
+  
   int a, b, c, d; 
   char slash;
 
+  // cout de las instrucciones
+  
   cout << "En este programa sumaremos dos fracciones.\n ";
   cout << "Escriba dos fracciones de forma a/b\n"; 
+
+  // le pedimos al usuario que ponga los numeros para fraccion 1 y fraccion 2
 
   cout << "Fraccion 1: ";
   cin >> a >> slash >> b;
 
   cout << "Fraccion 2: "; 
   cin >> c >> slash >> d; 
-
+  
+  // formula para sumar las fracciones 
+  
   int numerador = (a * d) + (b * c);
   int denominador = (b * d); 
 
+  // cout de la suma de las dos fracciones 
+  
   cout << "La suma de estas dos fracciones es: " << numerador << "/" << denominador << "\n"; 
   
   return 0;
